ad556f2597cd7527f02c01c711562b3b66d7dfa2
suvarnaks1 <suvarnaks2002@gmail.com>
2025-01-27 13:20:33 +0530

    messagespage

--- lib/common/circular_container.dart
+++ lib/common/circular_container.dart
@@ -1,3 +1,4 @@
+import 'package:buddypair/common/color_constants.dart';
 import 'package:flutter/material.dart';
 
 Widget circularContainer({
@@ -18,3 +19,22 @@ Widget circularContainer({
   );
 }
 
+//white circularcondainer
+
+Widget whitecCircularContainer({
+  required String imagePath,
+  required VoidCallback onPressed,
+}) {
+  return Container(
+    height: 40,
+    width: 40,
+    decoration: BoxDecoration(
+      border: Border.all(color: Colors.grey), // Adjust color as needed
+      borderRadius: BorderRadius.circular(100),
+    ),
+    child: IconButton(
+      onPressed: onPressed,
+      icon: Image.asset(imagePath,color: ColorConstants.whiteColor,), // Display the provided image
+    ),
+  );
+}
